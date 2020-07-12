80dcdb470f38a395a20fac6cf185aea574e86a93
Tuan Mai <tuandinhmai@gmail.com>
2020-07-12 04:33:28 +0000

    Added the untracked JUnit test file.

--- practice/src/test/java/ca/jrvs/practice/codingChallenge/DuplicatesFromSortedArrayTest.java
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/DuplicatesFromSortedArrayTest.java
@@ -2,11 +2,21 @@ package ca.jrvs.practice.codingChallenge;
 
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 public class DuplicatesFromSortedArrayTest {
 
     @Test
     public void dupsFromSortedArray() {
+        DuplicatesFromSortedArray dfsa = new DuplicatesFromSortedArray();
+        Set<Integer> expected = new HashSet<>();
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
+
+        assertEquals(expected, dfsa.dupsFromSortedArray(new int[] { 1, 1, 2, 2, 3, 3}));
     }
 }
