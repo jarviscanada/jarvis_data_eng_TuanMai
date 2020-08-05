21a8997bfeaf87a625cc8542753656dff2280ce1
Tuan Mai <tuandinhmai@gmail.com>
2020-08-05 15:55:28 +0000

    Configured Springboot Application

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
@@ -0,0 +1,26 @@
+package ca.jrvs.apps.trading;
+
+import ca.jrvs.apps.trading.model.config.MarketDataConfig;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+
+public class AppConfig {
+
+    private final Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
+    @Bean
+    public MarketDataConfig marketDataConfig() {
+        MarketDataConfig marketDataConfig = new MarketDataConfig();
+        marketDataConfig.setHost("https://cloud.iexapis.com/v1/");
+        marketDataConfig.setToken(System.getenv("IEX_PUB_TOKEN"));
+        return marketDataConfig;
+    }
+
+    @Bean
+    public HttpClientConnectionManager httpClientConnectionManager() {
+        return new PoolingHttpClientConnectionManager();
+    }
+}
