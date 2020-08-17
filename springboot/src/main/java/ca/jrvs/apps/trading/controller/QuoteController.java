3f64c153366f2d576c344dcfde9e187ea1ce66d9
Tuan Mai <tuandinhmai@gmail.com>
2020-08-17 20:25:45 +0000

    Completed Quote Controller Implementation.

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
@@ -1,15 +1,19 @@
 package ca.jrvs.apps.trading.controller;
 
 import ca.jrvs.apps.trading.model.domain.IexQuote;
+import ca.jrvs.apps.trading.model.domain.Quote;
 import ca.jrvs.apps.trading.service.QuoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/quote")
 public class QuoteController {
+
     private QuoteService quoteService;
 
     @Autowired
@@ -30,9 +34,43 @@ public class QuoteController {
 
     @PutMapping(path = "/iexMarketData")
     @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
     public void updateMarketData() {
         try {
             quoteService.updateMarketData();
+        } catch (Exception ex) {
+            throw ResponseExceptionUtil.getResponseStatusException(ex);
+        }
+    }
+
+    @PutMapping(path="/")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Quote putQuote(@RequestBody Quote quote) {
+        try {
+            return quoteService.saveQuote(quote);
+        } catch (Exception ex) {
+            throw ResponseExceptionUtil.getResponseStatusException(ex);
+        }
+    }
+
+    @PostMapping(path = "/tickerId/{tickerId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public Quote createQuote (@PathVariable String tickerId) {
+        try {
+            return quoteService.saveQuote(tickerId);
+        } catch (Exception ex) {
+            throw ResponseExceptionUtil.getResponseStatusException(ex);
+        }
+    }
+
+    @GetMapping(path = "/dailyList")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public List<Quote> getDailyList() {
+        try {
+            return quoteService.findAllQuotes();
         } catch (Exception ex) {
             throw ResponseExceptionUtil.getResponseStatusException(ex);
         }
